230228ba450c9cfeeb3e1c5863206634d73b879f
fanbaofang <1337138097@qq.com>
2023-03-23 16:58:22 +0800

    新增脚本

---
+++ test2/新增脚本.py
@@ -0,0 +1,15 @@
+#encoding:utf-8
+import sys
+
+#year = int(sys.argv[1])
+year = 2028
+if (year % 4) == 0:
+    if (year % 100) == 0:
+        if (year % 400) == 0:
+            print("{} 是闰年".format(year))  # 整百年能被400整除的是闰年
+        else:
+            print("{} 不是闰年".format(year))
+    else:
+        print("{} 是闰年".format(year))  # 非整百年能被4整除的为闰年
+else:
+    print("{} 不是闰年".format(year))
